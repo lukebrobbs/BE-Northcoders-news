8f716cf7967e7d2c2f7b9da38449617bac52da32
Luke <lukemichaelvocalist@gmail.com>
2018-04-01 11:20:49 +0100

    change config location

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = require("./server");
 const { PORT } =
   process.env.NODE_ENV === "production"
     ? process.env
-    : require("../config")[process.env.NODE_ENV];
+    : require("./config")[process.env.NODE_ENV];
 
 app.listen(PORT, function() {
   console.log(`listening on port ${PORT}`);
