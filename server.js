660c15a8e966dab829d66009735bd8eace5a54bd
Luke <lukemichaelvocalist@gmail.com>
2018-04-05 22:23:21 +0100

    add more cors

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
+    "Access-Control-Allow-Methods",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
   next();
