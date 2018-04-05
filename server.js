4a4550770e84c30908601b7d20662c3e7c35fd2c
Luke <lukemichaelvocalist@gmail.com>
2018-04-05 13:54:14 +0100

    add cors

--- server.js
+++ server.js
@@ -18,7 +18,14 @@ mongoose
 app.use(bodyParser.json());
 
 app.use(express.static("public"));
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use("/api", apiRouter);
 
 app.use("/*", (req, res) => {
